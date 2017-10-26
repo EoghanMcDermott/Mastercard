6c69d931f4126047e07c8815b0b9f448dfb51923
Eoghan <eoghan.mc-dermott.1@ucdconnect.ie>
2017-10-26 21:09:31 +0100

    Task 1 complete, starting work on task 2

---
+++ ReverseString.java
@@ -0,0 +1,13 @@
+public class ReverseString {
+
+    public String reverseIterative(String input)
+    {
+        return "";
+    }
+
+
+    public String reverseRecursive(String input, int stringLength)
+    {
+        return "";
+    }
+}
